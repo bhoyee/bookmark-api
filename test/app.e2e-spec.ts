62eda76c4e9b897f3d923fdf349d36910b6ed222
unknown <s.adeboye@outlook.com>
2024-05-08 10:29:40 +0100

    updated the prisma for end to end test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { AppModule } from "../src/app.module";
 import  {Test} from '@nestjs/testing';
+import { PrismaService } from "@prisma/prisma.service";
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = 
      await Test.createTestingModule({
@@ -16,12 +18,16 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService)
+
+    await prisma.cleanDb
   });
 
   afterAll(() => {
     app.close();
   });
 
-  
+
   it.todo('should pass');
 });
